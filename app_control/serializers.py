ca829bbecc637bf83004a23c96d2e980b1f03175
Mu'ayad Al Shareef <103497877+muayedjj@users.noreply.github.com>
2022-09-15 08:51:29 +0300

    Features: Shop, Invoice, Item | Updated: app_control/[admin.py, models.py, views.py] | Created: app_control/serializers.py

--- app_control/serializers.py
+++ app_control/serializers.py
@@ -1,4 +1,4 @@
-from .models import Inventory, InventoryGroup
+from .models import Inventory, InventoryGroup, Shop, Invoice, InvoiceItem
 from user_control.serializers import CustomUserSerializer
 from rest_framework import serializers
 
@@ -29,3 +29,63 @@ class InventorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventory
         fields = "__all__"
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    created_by = CustomUserSerializer(read_only=True)
+    created_by_id = serializers.CharField(write_only=True, required=False)
+    amount_total = serializers.CharField(read_only=True, required=False)
+    count_total = serializers.CharField(read_only=True, required=False)
+
+    class Meta:
+        model = Shop
+        fields = "__all__"
+
+
+class InvoiceItemSeriliazer(serializers.ModelSerializer):
+    invoice = serializers.CharField(read_only=True)
+    invoice_id = serializers.CharField(write_only=True)
+    item = InventorySerializer(read_only=True)
+    item_id = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = InvoiceItem
+        fields = "__all__"
+
+
+class InvoiceItemDataSerializer(serializers.Serializer):
+    item_id = serializers.CharField()
+    quantity = serializers.IntegerField()
+
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    created_by = CustomUserSerializer(read_only=True)
+    created_by_id = serializers.CharField(write_only=True, required=False)
+    shop = ShopSerializer(read_only=True)
+    shop_id = serializers.CharField(write_only=True)
+    invoice_items = InvoiceItemSeriliazer(read_only=True, many=True)
+    invoice_item_data = InvoiceItemDataSerializer(write_only=True, many=True)
+
+    class Meta:
+        model = Invoice
+        fields = "__all__"
+
+    def create(self, validated_data):
+        invoice_item_data = validated_data.pop("invoice_item_data")
+
+        if not invoice_item_data:
+            raise Exception("You need to provide at least one invoice item")
+
+        invoice = super().create(validated_data)
+
+        invoice_item_serializer = InvoiceItemSeriliazer(data=[
+            {"invoice_id": invoice.id, **item} for item in invoice_item_data
+        ], many=True)
+
+        if invoice_item_serializer.is_valid():
+            invoice_item_serializer.save()
+        else:
+            invoice.delete()
+            raise Exception(invoice_item_serializer.errors)
+
+        return invoice
